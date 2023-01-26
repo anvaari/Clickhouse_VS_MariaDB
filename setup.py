1f7a531ed4bdfaf6dc935dadfba1000464bf17df
Mohammad Anvaari <anvaari@pm.me>
2023-01-26 13:19:09 +0330

    Add setup.py
    setup file for project. It help discovery of modules

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 19:07:50 2023
+
+@author: anvaari
+"""
+
+from setuptools import setup, find_packages
+setup(
+    name = 'Clickhouse_VS_MariaDB',
+    description='Aim of this project is to show performance of clikchouse in OLAP operations',
+    author='Mohammad Anvaari',
+    packages = find_packages(),
+)
