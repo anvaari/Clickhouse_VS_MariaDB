0f3fbf8a31bd8466633f351f4a8949aff4b30e1d
Mohammad Anvaari <anvaari@pm.me>
2023-01-27 13:49:41 +0330

    Add functions
    - load_data_in_batch added. It load data in chunk_size batch in yield them
    - insert_data_into_db added. It insert given dataframe into database. It gives database as input
    - insert_post_type_into_db added. It insert post_type data into database

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ Created on Wed Jan 25 19:10:09 2023
 from utils.queries import clickhouse_tables , mariadb_tables
 from utils.clickhouse_utils import execute_query as click_exec , insert_into_clickhouse_table
 from utils.mariadb_utils import execute_query as maria_exec , insert_into_mariadb_table
-from etc.constants import clickhouse_cred , project_path , base_log_format , normal_datetime_format , mariadb_cred , posts_columns,profiles_columns,locations_columns , CLICKHOUSE,MARIADB
+from etc.constants import clickhouse_cred , project_path , base_log_format , normal_datetime_format , mariadb_cred , posts_columns,profiles_columns,locations_columns,post_types , CLICKHOUSE,MARIADB
+from etc.exceptions import DataNotFoundError
 
 import os
 import logging
@@ -37,4 +38,42 @@ def create_tables(database):
             logging.info(f"{table} created sucsessfuly in mariadb")
     else:
         raise ValueError(f"database must be on of '{CLICKHOUSE}' or '{MARIADB}' but {database} given.")
+
+
+def load_data_in_batch(chunk_size):
+    data_folder_path  = os.path.join(project_path,'data')
+    if not os.path.isdir(data_folder_path):
+        raise DataNotFoundError(f"All csv files should be in data folder in project path : {project_path}")
     
+    profiles_data_gen = pd.read_csv(os.path.join(data_folder_path, 'instagram_profiles.csv'),usecols=profiles_columns.keys(),chunksize=chunk_size)
+    for data in profiles_data_gen:
+        yield ('profiles',data)
+
+    location_data_gen = pd.read_csv(os.path.join(data_folder_path,'instagram_locations.csv'),usecols=locations_columns.keys(),chunksize=chunk_size)
+    for data in location_data_gen:
+        yield ('locations',data)
+        
+    post_data_gen = pd.read_csv(os.path.join(data_folder_path, 'instagram_posts.csv'),usecols=posts_columns.keys(),chunksize=chunk_size)
+    for data in post_data_gen:
+        yield ('posts',data)
+        
+def insert_data_into_db(data:pd.DataFrame,db:str,table_name):
+    insert_funcs = {CLICKHOUSE:insert_into_clickhouse_table,MARIADB:insert_into_mariadb_table}
+    credentials = {CLICKHOUSE:clickhouse_cred,MARIADB:mariadb_cred}
+    
+    data['insert_date'] = datetime.now()
+    insert_funcs[db](data,table_name,credentials[db])
+
+
+def insert_post_type_into_db(db:str):
+    insert_funcs = {CLICKHOUSE:insert_into_clickhouse_table,MARIADB:insert_into_mariadb_table}
+    credentials = {CLICKHOUSE:clickhouse_cred,MARIADB:mariadb_cred}
+
+    post_type_df = pd.DataFrame(columns=['id','name','insert_data'])
+    post_type_df['id'] = post_types.keys()
+    post_type_df['name'] = post_types.values()
+    post_type_df['insert_date'] = datetime.now()
+    
+    insert_funcs[db](post_type_df,'post_types',credentials[db])
+        
+    logging.info("All data loaded sucessfuly")
