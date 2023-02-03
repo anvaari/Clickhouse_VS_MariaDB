1eb2e6117cef1395d82d0f3f1e34cc3ff762d34b
Mohammad Anvaari <anvaari@pm.me>
2023-02-03 17:30:23 +0330

    Make columns nullable
    I found many null values in data. So we need columns to be nullable. It handled in this commit

--- utils/queries.py
+++ utils/queries.py
@@ -7,7 +7,7 @@ Created on Wed Jan 25 20:37:40 2023
 """
 
 post_table_create = """
-CREATE TABLE IF NOT EXISTS instagram.posts
+CREATE TABLE IF NOT EXISTS posts
 (
 
     `id` Int64,
@@ -18,11 +18,11 @@ CREATE TABLE IF NOT EXISTS instagram.posts
 
     `date` DateTime,
 
-    `post_type` Uint8,
+    `post_type` UInt8,
 
-    `like` Uint32,
+    `like` Nullable(UInt32),
 
-    `comments` UInt32,
+    `comments` Nullable(UInt32),
 
     `insert_date` DateTime,
 
@@ -33,20 +33,20 @@ SETTINGS index_granularity = 8192;
 """
 
 profile_table_create = """
-CREATE TABLE IF NOT EXISTS instagram.profiles
+CREATE TABLE IF NOT EXISTS profiles
 (
 
     `id` Int64,
 
-    `username` String,
+    `username` Nullable(String),
 
-    `name` String,
+    `name` Nullable(String),
 
-    `followings` Uint32,
+    `followings` Nullable(UInt32),
 
-    `followers` Uint32,
+    `followers` Nullable(UInt32),
 
-    `number_of_posts` Uint32,
+    `number_of_posts` Nullable(UInt32),
 
     `insert_date` DateTime,
 
@@ -56,16 +56,16 @@ ORDER BY (id)
 SETTINGS index_granularity = 8192;  
 """
 location_table_create = """
-CREATE TABLE IF NOT EXISTS instagram.locations
+CREATE TABLE IF NOT EXISTS locations
 (
 
     `id` Int64,
 
-    `name` String,
+    `name` Nullable(String),
 
-    `city` String,
+    `city` Nullable(String),
 
-    `country_code` FixedString(2),
+    `country_code` Nullable(FixedString(2)),
 
     `insert_date` DateTime,
 
@@ -75,12 +75,12 @@ ORDER BY (id)
 SETTINGS index_granularity = 8192;  
 """
 post_type_table_create = """
-CREATE TABLE IF NOT EXISTS instagram.post_types
+CREATE TABLE IF NOT EXISTS post_types
 (
 
-    `id` Uint8,
+    `id` UInt8,
 
-    `name` String,
+    `name` Nullable(String),
 
     `insert_date` DateTime
 
@@ -96,12 +96,12 @@ clickhouse_tables = {'posts':post_table_create,'profiles':profile_table_create,'
 post_table_create = """
 CREATE TABLE IF NOT EXISTS instagram.posts (
   `id` BIGINT NOT NULL  PRIMARY KEY,
-  `profile_id` BIGINT NOT NULL REFERENCES profiles(id),
-  `location_id` BIGINT NOT NULL REFERENCES locations(id),
-  `date` DATETIME NOT NULL,
-  `post_type` TINYINT NOT NULL REFERENCES post_types(id),
-  `like` INT UNSIGNED NOT NULL,
-  `comments` INT UNSIGNED NOT NULL,
+  `profile_id` BIGINT  REFERENCES profiles(id),
+  `location_id` BIGINT  REFERENCES locations(id),
+  `date` DATETIME ,
+  `post_type` TINYINT  REFERENCES post_types(id),
+  `like` INT UNSIGNED ,
+  `comments` INT UNSIGNED ,
   `insert_date` DATETIME
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -110,7 +110,7 @@ CREATE TABLE IF NOT EXISTS instagram.posts (
 profile_table_create = """
 
 CREATE TABLE IF NOT EXISTS instagram.profiles (
-  `id` BIGINT NOT NULL  PRIMARY KEY,
+  `id` BIGINT   PRIMARY KEY,
   `username` VARCHAR(2000) ,
   `name` VARCHAR(2000) ,
   `followings` INT UNSIGNED  ,
@@ -137,4 +137,3 @@ CREATE TABLE IF NOT EXISTS instagram.post_types (
 """
 
 mariadb_tables = {'profiles':profile_table_create,'locations':location_table_create,'post_types':post_type_table_create,'posts':post_table_create}
-
